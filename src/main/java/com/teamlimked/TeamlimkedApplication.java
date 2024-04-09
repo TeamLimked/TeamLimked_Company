fc9c5991714b4f2e6c32f52f0b2d761bd41a383c
RosieOh <20172207@gm.hannam.ac.kr>
2024-04-09 21:12:53 +0900

    💊Build : SpringBoot 3.1.6 기반 기본 세팅 작업💊

---
+++ src/main/java/com/teamlimked/TeamlimkedApplication.java
@@ -0,0 +1,13 @@
+package com.teamlimked;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamlimkedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeamlimkedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/teamlimked/TeamlimkedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamlimked;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamlimkedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
